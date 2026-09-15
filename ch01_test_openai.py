9d3c326d2423f47107fbd77eb6e7a15dba4caae3
notesolgae82 <kehdgur@kakao.com>
2026-09-15 10:21:23 +0900

    1장완료

--- ch01_test_openai.py
+++ ch01_test_openai.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY")
+    api_key=os.environ.get("OPENAI_API_KEY") # OS 환경변수 OPENAI_API_KEY 값 가져오기
 )
 
 chat_completion = client.chat.completions.create(
