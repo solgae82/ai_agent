094cd3b1a598b40e5d59e25ef7cf287fcd173133
solgae82 <kehdgur@gmail.com>
2026-09-16 16:48:23 +0900

    체이닝ui 완료

---
+++ utils.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import os
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') # # OS 환경변수 OPENAI_API_KEY 값 가져오기
+
+sysc_client = OpenAI(
+    api_key = OPENAI_API_KEY
+)
+
+# llm 호출 함수
+def llm_call(prompt: str, model: str = 'gpt-4o-mini') -> str: 
+    messages = []
+    messages.append({'role': 'user', 'content': prompt})
+
+    chat_completion = sysc_client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+
+    return chat_completion.choices[0].message.content
+
+# 모듈이 아닌 main으로 호출할때..
+if __name__ == "__main__": 
+    test = llm_call("한국 수도는?")
+    print(test)
